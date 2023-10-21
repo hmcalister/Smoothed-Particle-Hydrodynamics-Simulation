caee1b0ba0d77afc8deeacc5eaf2d5846823f48b
Hayden McAlister <haydenmcalister49@gmail.com>
2023-10-22 00:57:00 +1300

    Add functions to create default config and read config file

---
+++ config/configReader.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"os"
+
+	"github.com/creasty/defaults"
+	"gopkg.in/yaml.v3"
+)
+
+func CreateDefaultConfig() *SimulationConfig {
+	defaultConfig := &SimulationConfig{}
+	defaults.Set(defaultConfig)
+	defaultConfig.finalizeConfig()
+
+	return defaultConfig
+}
+
+func ReadConfigYaml(yamlFilePath string) (*SimulationConfig, error) {
+	// Ensure the file exists and read it
+
+	fileContents, err := os.ReadFile(yamlFilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	// simulationConfig := &SimulationConfig{}
+	simulationConfig := CreateDefaultConfig()
+
+	// Unmarshal the file contents into a SimulationConfig struct
+	err = yaml.Unmarshal(fileContents, simulationConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	simulationConfig.finalizeConfig()
+	return simulationConfig, nil
+}
