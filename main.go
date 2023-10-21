98847ccb7e1a2cebf281d9a0cc6b0693be810605
Hayden McAlister <haydenmcalister49@gmail.com>
2023-10-22 00:57:16 +1300

    Update main.go with config file reading

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"hmcalister/SmoothedParticleHydrodynamicsSimulation/config"
+	"log"
+)
+
+var (
+	SimulationConfigSettings *config.SimulationConfig
+)
+
+func init() {
+	configFilePath := flag.String("ConfigFile", "", "Path to the config file. No path results in default config.")
+	flag.Parse()
+
+	if *configFilePath == "" {
+		SimulationConfigSettings = config.CreateDefaultConfig()
+	} else {
+		var err error
+		SimulationConfigSettings, err = config.ReadConfigYaml(*configFilePath)
+		if err != nil {
+			log.Panicf("error during reading config file: %v", err)
+		}
+	}
+}
 
 func main() {
-	log.Println("Hello, World!")
+	log.Printf("%+v", SimulationConfigSettings)
 }
