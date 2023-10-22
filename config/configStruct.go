e56142d1dfbfa2fd47505833d248c5b710369e57
Hayden McAlister <haydenmcalister49@gmail.com>
2023-10-22 16:14:41 +1300

    Update config struct to allow for random seed, particle size

--- config/configStruct.go
+++ config/configStruct.go
@@ -1,14 +1,20 @@
 package config
 
-import "math"
+import (
+	"math"
+	"math/rand"
+)
 
 type SimulationConfig struct {
 	// Simulation Config --------------------------------------------------------------------------
 
 	NumParticles       int     `default:"1000" yaml:"NumParticles"`
 	ParticleMass       float64 `default:"1.0" yaml:"ParticleMass"`
+	ParticleSize       int32   `default:"5" yaml:"ParticleSize"`
 	FluidTargetDensity float64 `default:"1.0" yaml:"FluidTargetDensity"`
 	GravityStrength    float64 `default:"1.0" yaml:"GravityStrength"`
+	// If random seed is set to 0, then a random seed is generated instead
+	RandomSeed uint64 `default:"0" yaml:"RandomSeed"`
 
 	// Smoothing Kernel Config --------------------------------------------------------------------
 
@@ -18,7 +24,7 @@ type SimulationConfig struct {
 
 	// GUI Config ---------------------------------------------------------------------------------
 
-	SimulationWidth  int32 `default:"512" yaml:"SimulationWidth"`
+	SimulationWidth  int32 `default:"1024" yaml:"SimulationWidth"`
 	SimulationHeight int32 `default:"512" yaml:"SimulationHeight"`
 	// The padding to add around the simulation for nice visualizations .
 	// Note this is added to the simulation width and height such that
@@ -42,5 +48,9 @@ func (simulationConfig *SimulationConfig) finalizeConfig() {
 	if simulationConfig.SpatialHashingBins == -1 {
 		simulationCoveringBins := (1 + float64(simulationConfig.SimulationWidth)/simulationConfig.SmoothingKernelRadius) * (1 + float64(simulationConfig.SimulationHeight)/simulationConfig.SmoothingKernelRadius)
 		simulationConfig.SpatialHashingBins = int(math.Ceil(simulationCoveringBins))
+	}
+
+	if simulationConfig.RandomSeed == 0 {
+		simulationConfig.RandomSeed = rand.Uint64()
 	}
 }
