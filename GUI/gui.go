57040f31de13c67c149434cfc2709ef6b115419f
Hayden McAlister <haydenmcalister49@gmail.com>
2023-10-22 16:15:47 +1300

    Add surface to gui config struct

--- GUI/gui.go
+++ GUI/gui.go
@@ -2,6 +2,7 @@ package gui
 
 import (
 	"hmcalister/SmoothedParticleHydrodynamicsSimulation/config"
+	"hmcalister/SmoothedParticleHydrodynamicsSimulation/particle"
 	"log"
 
 	"github.com/veandco/go-sdl2/sdl"
@@ -10,6 +11,7 @@ import (
 type GUIConfig struct {
 	simulationConfig *config.SimulationConfig
 	window           *sdl.Window
+	surface          *sdl.Surface
 }
 
 func InitGUI(simulationConfig *config.SimulationConfig) (*GUIConfig, error) {
@@ -23,8 +25,13 @@ func InitGUI(simulationConfig *config.SimulationConfig) (*GUIConfig, error) {
 	}
 
 	guiConfig.window, err = sdl.CreateWindow("Smoothed Particle Hydrodynamics", sdl.WINDOWPOS_CENTERED, sdl.WINDOWPOS_CENTERED,
-		simulationConfig.SimulationWidth+simulationConfig.SimulationPadding, simulationConfig.SimulationHeight+simulationConfig.SimulationPadding,
+		simulationConfig.SimulationWidth+2*simulationConfig.SimulationPadding, simulationConfig.SimulationHeight+2*simulationConfig.SimulationPadding,
 		sdl.WINDOW_SHOWN)
+	if err != nil {
+		return nil, err
+	}
+
+	guiConfig.surface, err = guiConfig.window.GetSurface()
 	if err != nil {
 		return nil, err
 	}
