edaff652f26adc3e6cf532c8d53e2ef88e06b869
Hayden McAlister <haydenmcalister49@gmail.com>
2023-10-22 16:14:12 +1300

    Add method to create collection of particles randomly

--- particle/particle.go
+++ particle/particle.go
@@ -18,3 +18,21 @@ type ParticleCollection struct {
 	Particles        []*Particle
 }
 
+func CreateInitialParticles(simulationConfig *config.SimulationConfig) *ParticleCollection {
+	particleCollection := &ParticleCollection{}
+	particleCollection.simulationConfig = simulationConfig
+	particleCollection.Particles = make([]*Particle, simulationConfig.NumParticles)
+
+	particleCollection.rng = rand.New(rand.NewSource(simulationConfig.RandomSeed))
+
+	for particleIndex := 0; particleIndex < simulationConfig.NumParticles; particleIndex += 1 {
+		particleX := float64(simulationConfig.SimulationWidth) * particleCollection.rng.Float64()
+		particleY := float64(simulationConfig.SimulationHeight) * particleCollection.rng.Float64()
+		particleCollection.Particles[particleIndex] = &Particle{
+			Position: *mat.NewVecDense(2, []float64{particleX, particleY}),
+			Velocity: *mat.NewVecDense(2, nil),
+		}
+	}
+
+	return particleCollection
+}
