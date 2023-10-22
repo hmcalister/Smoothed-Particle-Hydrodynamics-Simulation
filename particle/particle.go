c9a45207bd3be3f50e28befc16960c4d61470f8b
Hayden McAlister <haydenmcalister49@gmail.com>
2023-10-22 16:13:58 +1300

    Add struct to hold collection of particles

--- particle/particle.go
+++ particle/particle.go
@@ -1,8 +1,20 @@
 package particle
 
-import "gonum.org/v1/gonum/mat"
+import (
+	"hmcalister/SmoothedParticleHydrodynamicsSimulation/config"
+
+	"golang.org/x/exp/rand"
+	"gonum.org/v1/gonum/mat"
+)
 
 type Particle struct {
 	Position mat.VecDense
 	Velocity mat.VecDense
 }
+
+type ParticleCollection struct {
+	rng              *rand.Rand
+	simulationConfig *config.SimulationConfig
+	Particles        []*Particle
+}
+
