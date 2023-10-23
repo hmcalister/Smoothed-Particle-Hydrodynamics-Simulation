4d089f918777f7f1cd2cc60d1e93334f30c54517
Hayden McAlister <haydenmcalister49@gmail.com>
2023-10-23 15:01:09 +1300

    Update bug in smoothing kernel

--- particle/smoothingKernel.go
+++ particle/smoothingKernel.go
@@ -7,7 +7,7 @@ func normalizationCoefficient(smoothingRadius float64, exponent float64) float64
 }
 
 func smoothingKernel(displacement float64, smoothingRadius float64, exponent float64) float64 {
-	return normalizationCoefficient(smoothingRadius, exponent) * math.Pow(displacement, 2)
+	return normalizationCoefficient(smoothingRadius, exponent) * math.Pow(displacement, exponent)
 }
 
 func smoothingKernelGradientMagnitude(displacement float64, smoothingRadius float64, exponent float64) float64 {
