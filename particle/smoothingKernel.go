62c851a2b055fec661e73fe19a71a01499bf0775
Hayden McAlister <haydenmcalister49@gmail.com>
2023-10-23 14:15:55 +1300

    Add smoothing kernel implementation

---
+++ particle/smoothingKernel.go
@@ -0,0 +1,15 @@
+package particle
+
+import "math"
+
+func normalizationCoefficient(smoothingRadius float64, exponent float64) float64 {
+	return ((exponent*exponent + 3*exponent + 2) / (2 * math.Pi * math.Pow(smoothingRadius, exponent+2)))
+}
+
+func smoothingKernel(displacement float64, smoothingRadius float64, exponent float64) float64 {
+	return normalizationCoefficient(smoothingRadius, exponent) * math.Pow(displacement, 2)
+}
+
+func smoothingKernelGradientMagnitude(displacement float64, smoothingRadius float64, exponent float64) float64 {
+	return exponent * normalizationCoefficient(smoothingRadius, exponent) * math.Pow(displacement, exponent-1)
+}
