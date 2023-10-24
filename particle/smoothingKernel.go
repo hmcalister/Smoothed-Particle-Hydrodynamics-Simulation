1b785f7bbc4e6d1e2d3d6f1e97c2858a2da45c83
Hayden McAlister <haydenmcalister49@gmail.com>
2023-10-25 09:58:50 +1300

    Bug fix smoothing kernel displacement radius

--- particle/smoothingKernel.go
+++ particle/smoothingKernel.go
@@ -1,15 +1,24 @@
 package particle
 
-import "math"
+import (
+	"math"
+)
 
-func normalizationCoefficient(smoothingRadius float64, exponent float64) float64 {
-	return ((exponent*exponent + 3*exponent + 2) / (2 * math.Pi * math.Pow(smoothingRadius, exponent+2)))
+const (
+	smoothingKernelRadius float64 = 20.0
+	exponent              float64 = 2
+)
+
+func normalizationCoefficient() float64 {
+	return ((exponent*exponent + 3*exponent + 2) / (2 * math.Pi * math.Pow(smoothingKernelRadius, exponent+2)))
 }
 
-func smoothingKernel(displacement float64, smoothingRadius float64, exponent float64) float64 {
-	return normalizationCoefficient(smoothingRadius, exponent) * math.Pow(displacement, exponent)
+func smoothingKernel(displacement float64) float64 {
+	displacement = min(displacement, smoothingKernelRadius)
+	return normalizationCoefficient() * math.Pow(smoothingKernelRadius-displacement, exponent)
 }
 
-func smoothingKernelGradientMagnitude(displacement float64, smoothingRadius float64, exponent float64) float64 {
-	return exponent * normalizationCoefficient(smoothingRadius, exponent) * math.Pow(displacement, exponent-1)
+func smoothingKernelGradientMagnitude(displacement float64) float64 {
+	displacement = min(displacement, smoothingKernelRadius)
+	return exponent * normalizationCoefficient() * math.Pow(smoothingKernelRadius-displacement, exponent-1)
 }
